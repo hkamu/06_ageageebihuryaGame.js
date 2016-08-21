e1594118f806780d8ac9f306c340b09ad7ac28c1
KanamuHiguchi <www.nanasi.com@gmail.com>
2016-08-21 13:43:58 +0900

    サンゴにぶつかると残機が減る＆エビ君堂々爆誕

--- src/resource.js
+++ src/resource.js
@@ -1,10 +1,19 @@
+
 var res = {
     background_png : "res/background.png",
-    ship_png : "res/ship.png",
+    ship_png : "res/shrimp01.png",
     particle_png : "res/particle.png",
-    asteroid_png : "res/asteroid.png",
-    bgm_main : "res/09-XENO-Factor.mp3",
-    se_bang : "res/se_bang.mp3"
+    asteroid_png : "res/coral_under.png",
+    asteroid02_png : "res/coral_above.png",
+    bgm_main : "res/bgm_main.mp3",
+    se_bang : "res/se_bang.mp3",
+    title_png : "res/Title.png",
+    replay_png : "res/replay_button.png",
+    gameOV_png : "res/gameover.png",
+    gameST_png : "res/start.png",
+    ship02_png : "res/shrimp02.png",
+    ship03_png : "res/shrimp03.png",
+    ship04_png : "res/shrimp04.png"
 };
 
 var g_resources = [];
